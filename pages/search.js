2bd27469cd1bbc078c6b9ea17a517696f4f5d9d9
usoock <usoock13@gmail.com>
2021-08-07 19:33:43 +0900

    fetch 시도 중

--- pages/search.js
+++ pages/search.js
@@ -2,14 +2,33 @@ import React from "react";
 import { useRouter } from "next/router";
 import Layout from "../components/Layout";
 
-const Search = ({ url }) => {
-    const router = useRouter();
-    console.log(router.query.keyword);
-    return (
-        <Layout>
-            input keyword is :: "{router.query.keyword}"
-        </Layout>
-    )
+// const Search = ({ url }) => {
+//     return (
+//         <Layout>
+//             <SSRTest />
+//         </Layout>
+//     )
+// }
+
+class Search extends React.Component {
+    static async getInitialProps ({ req }) {
+        fetch("https://jsonplaceholder.typicode.com/users")
+        .then(res => {
+            res.blob();
+        })
+        .then(blob => {
+            console.log(blob);
+        });
+
+        return req ? { from : "server" } : { from : "client" };
+    }
+    render() {
+        return (
+            <Layout>
+                {this.props.from}측 실행
+            </Layout>
+        )
+    }
 }
 
 export default Search;
